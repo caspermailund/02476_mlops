8a78ee3614785c716c6c68fa392233cb519f203e
Nullerh <peter.f.hulgaard@gmail.com>
2025-01-16 13:20:24 +0100

    tests v1

---
+++ tests/test_model.py
@@ -0,0 +1,8 @@
+import torch
+from image_classfication.model import SimpleCNN
+
+def test_model():
+    model = SimpleCNN()
+    x = torch.randn(1, 3, 256, 256)
+    y = model(x)
+    assert y.shape == (1, 14)
